00573bb771ae3b221f2a2b450ad75f944d00765c
huangql <huangql@szlanyou.com>
2022-03-10 10:38:37 +0800

    添加关键点说明

--- app/src/main/java/com/hql/databaseandprovider/WeatherContentProvider.java
+++ app/src/main/java/com/hql/databaseandprovider/WeatherContentProvider.java
@@ -21,7 +21,7 @@ public class WeatherContentProvider extends ContentProvider {
     private static final String AUTHORITIES = "com.hql.provider";
     private static final String PATH = "com.hql.test.service.weatherdb";
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITIES);
-    public static final Uri CONTENT_URI_TEST = Uri.parse("content://" + "com.hql.test.service.weather.info.provider");
+    //public static final Uri CONTENT_URI_TEST = Uri.parse("content://" + "com.hql.test.service.weather.info.provider");
     private static final UriMatcher mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     private static final int URI_CODE = 1;
 
@@ -42,13 +42,16 @@ public class WeatherContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        SQLiteDatabase db = mWeatherDataBase.getWritableDatabase();
-       // SQLiteDatabase db = new WeatherDataBase(getContext()).getWritableDatabase();
-        Cursor cursor = db.query(Constants.TABLE_WEATHER, projection, selection, selectionArgs, null, null, null);
-        //Cursor cursor =   db.query(Constants.TABLE_WEATHER, new String[]{Constants.LABEL_CITY_ID}, Constants.LABEL_CITY_ID + " = ?", new String[]{("707")}, null, null, null);
+        if (mUriMatcher.match(uri) == URI_CODE) {
+            SQLiteDatabase db = mWeatherDataBase.getWritableDatabase();
+            // SQLiteDatabase db = new WeatherDataBase(getContext()).getWritableDatabase();
+            Cursor cursor = db.query(Constants.TABLE_WEATHER, projection, selection, selectionArgs, null, null, null);
+            //Cursor cursor =   db.query(Constants.TABLE_WEATHER, new String[]{Constants.LABEL_CITY_ID}, Constants.LABEL_CITY_ID + " = ?", new String[]{("707")}, null, null, null);
 
-        Log.d(TAG,"数据库 查询 cursor>>>>>>>"+cursor.getCount());
-        return cursor;
+            Log.d(TAG, "数据库 查询 cursor>>>>>>>" + cursor.getCount());
+            return cursor;
+        }
+        return null;
     }
 
     @Nullable
@@ -60,13 +63,16 @@ public class WeatherContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        SQLiteDatabase db = mWeatherDataBase.getWritableDatabase();
-        long row = db.insert(Constants.TABLE_WEATHER, null, values);
-        if (row != -1) {
-            getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+        if (mUriMatcher.match(uri) == URI_CODE) {
+            SQLiteDatabase db = mWeatherDataBase.getWritableDatabase();
+            long row = db.insert(Constants.TABLE_WEATHER, null, values);
+            if (row != -1) {
+                getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+            }
+            Log.d(TAG, "数据库 插入 cursor>>>>>>>");
+            return CONTENT_URI;
         }
-        Log.d(TAG,"数据库 插入 cursor>>>>>>>");
-        return CONTENT_URI;
+        return null;
     }
 
     @Override
@@ -76,14 +82,17 @@ public class WeatherContentProvider extends ContentProvider {
 
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        SQLiteDatabase db = mWeatherDataBase.getWritableDatabase();
-        //SQLiteDatabase db = new WeatherDataBase(getContext()).getWritableDatabase();
-        //db.update(Constants.TABLE_WEATHER, values, Constants.LABEL_CITY_ID + " = ?", args);
-        int row = db.update(Constants.TABLE_WEATHER, values, selection + " = ?", selectionArgs);
-        if (row != -1) {
-            getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+        if (mUriMatcher.match(uri) == URI_CODE) {
+            SQLiteDatabase db = mWeatherDataBase.getWritableDatabase();
+            //SQLiteDatabase db = new WeatherDataBase(getContext()).getWritableDatabase();
+            //db.update(Constants.TABLE_WEATHER, values, Constants.LABEL_CITY_ID + " = ?", args);
+            int row = db.update(Constants.TABLE_WEATHER, values, selection + " = ?", selectionArgs);
+            if (row != -1) {
+                getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+            }
+            Log.d(TAG, "数据库 更新 cursor>>>>>>>");
+            return row;
         }
-        Log.d(TAG,"数据库 更新 cursor>>>>>>>");
-        return row;
+        return 0;
     }
 }
